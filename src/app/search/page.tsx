05bad8ec601fe72c6845a3c75e372007079b098b
Dirivial <Alexander.Kadeby@gmail.com>
2025-04-05 13:41:16 +0200

    Add suspense around search component

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,6 @@
 import { HydrateClient } from "~/trpc/server";
 import { SearchComponent } from "../_components/search";
+import { Suspense } from "react";
 
 export default async function Home() {
   return (
@@ -7,7 +8,13 @@ export default async function Home() {
       <main className="h-screen py-2">
         <div className="flex flex-col items-center justify-center gap-4">
           <div className="w-full max-w-2xl">
-            <SearchComponent />
+            <Suspense
+              fallback={
+                <div className="flex w-full flex-col gap-4">Hello brothers</div>
+              }
+            >
+              <SearchComponent />
+            </Suspense>
           </div>
         </div>
       </main>
